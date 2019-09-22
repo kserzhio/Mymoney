68bbcb6dcfece4f6dac159aef6a379488e50ef39
kserzhio <kserzhio@gmail.com>
2019-09-22 17:05:45 +0300

    create login module add validation form and create routing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    AuthModule,
     AppRoutingModule
   ],
   providers: [],
